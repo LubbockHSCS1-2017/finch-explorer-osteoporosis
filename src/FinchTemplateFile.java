4a6fe9b58bca7939afbb362baff07ea2f0d9e9df
James Spann <james.m.spann.2@gmail.com>
2017-01-17 08:34:40 -0600

    i maed dis

--- src/FinchTemplateFile.java
+++ src/FinchTemplateFile.java
@@ -3,8 +3,8 @@
 import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 
 /**
- * Created by:
- * Date:
+ * Created by: James Spann
+ * Date: 1/12/2016
  * A starter file to use the Finch
  */
 
@@ -16,6 +16,18 @@ public class FinchTemplateFile
       Finch myFinch = new Finch();
 
       // Write some code here!
+      
+      myFinch.setLED(0, 255, 255);
+      myFinch.setWheelVelocities(100, -100, 4000);
+      myFinch.setWheelVelocities(255, 255, 1000);
+      myFinch.setWheelVelocities(100, -100, 4000);
+      myFinch.setWheelVelocities(255, 255, 1000);
+      myFinch.setWheelVelocities(100, -100, 4000);
+      myFinch.setWheelVelocities(255, 255, 1000);
+      myFinch.setWheelVelocities(100, -100, 4000);
+      myFinch.setWheelVelocities(255, 255, 1000);
+      myFinch.setWheelVelocities(-100, 100, 800);
+      myFinch.sleep(900000);
 
       // Always end your program with finch.quit()
       myFinch.quit();
